62f0535b65675aae98e2efe521b5b08d26adb71b
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-12-01 23:38:48 +0700

    20. render stars for detail game

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -14,6 +14,9 @@ import xbox from "../img/xbox.svg";
 import nintendo from "../img/nintendo.svg";
 import apple from "../img/apple.svg";
 import gamepad from "../img/gamepad.svg";
+// Star images
+import starEmpty from "../img/star-empty.png";
+import starFull from "../img/star-full.png";
 
 const GameDetail = ({ pathId }) => {
   const history = useHistory();
@@ -51,6 +54,20 @@ const GameDetail = ({ pathId }) => {
         return gamepad;
     }
   };
+  // Get stars
+  const getStars = () => {
+    const stars = [];
+    const rating = Math.floor(game.rating);
+
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push(<img alt="star" key={i} src={starFull} />);
+      } else {
+        stars.push(<img alt="star" key={i} src={starEmpty} />);
+      }
+    }
+    return stars;
+  };
 
   // Data
   const { screen, game, isLoading } = useSelector((state) => state.detail);
@@ -63,7 +80,7 @@ const GameDetail = ({ pathId }) => {
             <Stats className="stats">
               <div className="rating">
                 <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
-                <p>Rating: {game.rating}</p>
+                {getStars()}
               </div>
               <Info className="info">
                 <h3>Platforms</h3>
@@ -143,6 +160,12 @@ const Stats = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  img {
+    width: 2rem;
+    height: 2rem;
+    display: inline;
+  }
 `;
 
 const Info = styled(motion.div)`
